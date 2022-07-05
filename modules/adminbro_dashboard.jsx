f42916ac86e97d610cf4b2652ba7d26cb8953d3d
Asher <asher.haun@outlook.com>
2022-07-05 13:17:59 -0500

    react dashboard

--- modules/adminbro_dashboard.jsx
+++ modules/adminbro_dashboard.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
 
 const Dashboard = (props) => {
+  const aStyle = {
+    textDecoration: "none",
+    color: "white",
+    top: "200px",
+    background: "#738FF9",
+    padding: "10px",
+    marginLeft: "10px",
+    borderRadius: "5px"
+  }
+  const svgStyle = {
+    width: "1em",
+    height: "1em"
+  }
   return (
     <div>
+      <br />
+      <br />
+      <div style={{marginLeft: "10px"}}>
+        <a style={aStyle} target="blank" href="https://reflowhq.com/stores/1316370211">
+          <h3 style={{ display: "inline" }}>
+            <span style={{ marginRight: "10px" }}>
+              <svg xmlns="http://www.w3.org/2000/svg" style={svgStyle} fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+                <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z" />
+              </svg>
+            </span>
+            Manage Store
+          </h3>
+        </a>
+
+        <a style={aStyle} href="/admin/resources/Page">
+          <h3 style={{ display: "inline" }}>
+            <span style={{ marginRight: "10px" }}>
+            <svg xmlns="http://www.w3.org/2000/svg" style={svgStyle} fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+              <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+            </svg>
+            </span>
+            Manage Pages
+          </h3>
+        </a>
+
+        <a style={aStyle} href="/admin/resources/User">
+          <h3 style={{ display: "inline" }}>
+            <span style={{ marginRight: "10px" }}>
+            <svg xmlns="http://www.w3.org/2000/svg" style={svgStyle} fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+              <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+            </svg>
+            </span>
+            Manage Users
+          </h3>
+        </a>
+      </div>
     </div>
   )
 }
